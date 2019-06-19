8b0cec1139f5aa1e1468393ccffb6894a9eabc96
fudong.yang <fudong.yang@socialcredits.cn>
2019-06-19 17:36:23 +0800

    add sql data

---
+++ src/main/doc/_user__201906191734.sql
@@ -0,0 +1,3 @@
+INSERT INTO store.`user` (password,name) VALUES 
+('123456','fudo')
+;
---
+++ src/main/doc/stock_201906191734.sql
@@ -0,0 +1,4 @@
+INSERT INTO store.stock (goods_id,total,remains,sells,cost,profit,create_time,modify_time) VALUES 
+(2,200,0,200,2000,4000,'2019-06-19 09:14:20.000','2019-06-19 09:14:20.000')
+,(1,200,0,200,20000,6000,'2019-06-19 09:14:20.000','2019-06-19 09:14:20.000')
+;
