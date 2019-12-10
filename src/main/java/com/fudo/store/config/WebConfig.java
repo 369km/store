55cd8a279bbf724702b2385ff814a0d405d405e8
fudong.yang <fudong.yang@socialcredits.cn>
2019-12-10 22:27:31 +0800

    fix

--- src/main/java/com/fudo/store/config/WebConfig.java
+++ src/main/java/com/fudo/store/config/WebConfig.java
@@ -19,6 +19,6 @@ public class WebConfig implements WebMvcConfigurer  {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns(Arrays.asList("/", "/user/login", "/user/logout"));
+                .excludePathPatterns(Arrays.asList("/", "/user/login", "/user/logout","/static/**"));
     }
 }
--- src/main/java/com/fudo/store/controller/UserController.java
+++ src/main/java/com/fudo/store/controller/UserController.java
@@ -4,11 +4,13 @@ import com.fudo.store.model.User;
 import com.fudo.store.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
 
 @Controller
 @RequestMapping("/user")
